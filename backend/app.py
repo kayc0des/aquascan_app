f86c77c2f2a2a1b78ecc05d351f304711c537b6e
kayc0des <k.boafo@alustudent.com>
2024-06-10 23:40:13 +0200

    node modules

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 app = FastAPI()
 
 # Load the trained model
-model = tf.keras.models.load_model('model/model.h5')
+model = tf.keras.models.load_model('model.h5')
 
 # Mean and standard deviation for normalization
 mean = np.array([7.08079450e+00, 1.96369496e+02, 2.20140925e+04, 7.12227679e+00,
